d49c9e4d8783ca5b8377ef62714b9d2b3e52a652
Zaid mohamad <zaidaltamari5@gmail.com>
2023-07-31 18:49:35 +0400

    yaa

--- real_shop/lib/providers/auth.dart
+++ real_shop/lib/providers/auth.dart
@@ -71,7 +71,30 @@ class Auth with ChangeNotifier {
   }
 
   Future<void> signUp(String email, String password) async {
-    return _authenticate(email, password, 'signUp');
+    await _authenticate(email, password, 'signUp');
+    _sendEmailVerification();
+  }
+
+  Future<void> _sendEmailVerification() async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAgxjGMH8T9Ltgb8xGpxUi84xVC0h5jEd4';
+    try {
+      final res = await http.post(
+        url,
+        body: json.encode(
+          {
+            'requestType': 'VERIFY_EMAIL',
+            'idToken': _token,
+          },
+        ),
+      );
+      final responseData = json.decode(res.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (e) {
+      throw e;
+    }
   }
 
   Future<void> login(String email, String password) async {
